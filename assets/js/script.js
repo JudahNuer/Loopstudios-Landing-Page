04f97ff0eb2c95434d508547709d1b36ea8beb6e
JudahNuer <91411239+JudahNuer@users.noreply.github.com>
2022-08-29 18:46:39 +0000

    javascript fns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//navbar buttons variables
+const hamburger = document.querySelector('#open-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+//navbar variables
+const navBar = document.querySelector('.navbar-nav');
+
+// navbar toggle function
+const navbarToggle = () => navBar.classList.toggle('active');
+
+// added toggle function on navbar
+hamburger.addEventListener('click', navbarToggle);
+closeBtn.addEventListener(`click`, navbarToggle);
